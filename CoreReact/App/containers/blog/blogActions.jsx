ac77f9b873f4215b69c91cbcaca146b4ce3601b5
E-LAB\Mikhail.Nazarov <Mikhail.Nazarov@e-lab.icl.kazan.ru>
2018-03-16 10:14:04 +0300

    Init asp.net core 2.0 + react app.

---
+++ CoreReact/App/containers/blog/blogActions.jsx
@@ -0,0 +1,34 @@
+﻿import { GET_POSTS_SUCCESS, GET_POSTS_ERROR } from './blogConstans.jsx';
+import "isomorphic-fetch";
+import { constants } from 'zlib';
+
+export function receivePosts(data) {
+    return {
+        type: GET_POSTS_SUCCESS,
+        posts: data
+    }
+}
+
+export function errorReceive(err) {
+    return {
+        type: GET_POSTS_ERROR,
+        error: err
+    }
+}
+
+export function getPosts(pageIndex = 0, tag) {
+    return (dispatch) => {
+        let queryTrailer = '?pageIndex=' + pageIndex;
+        if (tag) {
+            queryTrailer += '&tag=' + tag;
+        }
+        fetch(constants.getPage + queryTrailer)
+            .then((response) => {
+                return response.json()
+            }).then((data) => {
+                dispatch(receivePosts(data))
+            }).catch((ex) => {
+                dispatch(errorReceive(err))
+            });
+    }
+}
---
+++ CoreReact/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CoreReact
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "DefaultApi",
+                    template: "api/{controller}/{action}");
+                routes.MapSpaFallbackRoute("spa-fallback", new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
